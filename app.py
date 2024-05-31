1b12d2e508f0e420d421516f05985fe312420633
s22595 <sieczeklol@gmail.com>
2024-05-31 21:40:49 +0200

    Done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st 
+import streamlit as st
 import pickle
 from datetime import datetime
 
@@ -7,8 +7,7 @@ startTime = datetime.now()
 
 # Load the pre-trained model
 filename = "model.h5"
-with open(filename, 'rb') as file:
-    model = pickle.load(file)
+model = pickle.load(open(filename, 'rb'))
 
 # Define dictionaries for labels
 sex_d = {0: "Kobieta", 1: "Mężczyzna"}
@@ -22,8 +21,7 @@ def main():
     prediction = st.container()
 
     st.image(
-        "https://media1.popsugar-assets.com/files/thumbor/7CwCuGAKxTrQ4wPyOBpKjSsd1JI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/04/19/743/n/41542884/5429b59c8e78fbc4_MCDTITA_FE014_H_1_.JPG"
-    )
+        "https://media1.popsugar-assets.com/files/thumbor/7CwCuGAKxTrQ4wPyOBpKjSsd1JI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/04/19/743/n/41542884/5429b59c8e78fbc4_MCDTITA_FE014_H_1_.JPG")
 
     with overview:
         st.title("Czy przeżyłbyś katastrofę?")
@@ -31,7 +29,8 @@ def main():
     with left:
         sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
         pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
-        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2, format_func=lambda x: embarked_d[x])
+        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2,
+                                  format_func=lambda x: embarked_d[x])
 
     with right:
         age_slider = st.slider("Wiek", value=50, min_value=1, max_value=100)
@@ -39,16 +38,18 @@ def main():
         parch_slider = st.slider("# Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
         fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=10)
 
-    # Collect input data for prediction
-    data = [[pclass_radio, sex_radio, age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
+ # Collect input data for prediction
+    data = [sex_radio,pclass_radio,embarked_radio,age_slider,sibsp_slider,parch_slider,fare_slider]
+    
+    data = [data]
 
     # Make prediction
     survival = model.predict(data)
     s_confidence = model.predict_proba(data)
 
     with prediction:
-        st.header(f"Czy dana osoba przeżyje? {'Tak' if survival[0] == 1 else 'Nie'}")
-        st.subheader(f"Pewność predykcji {s_confidence[0][survival][0] * 100:.2f} %")
+        st.header("Czy dana osoba przeżyje? {0}".format("Tak" if survival[0] == 1 else "Nie"))
+        st.subheader("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival[0]] * 100))
 
 if __name__ == "__main__":
     main()
